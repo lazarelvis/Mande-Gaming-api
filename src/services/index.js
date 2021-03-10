d67de1b87030cc728c4cc01c9aa29778de1532f5
Elvis lazar <lazarelvis15@gmail.com>
2021-03-10 19:48:01 +0200

    games service api aded

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const user = require('./user/user.service.js');
+const games = require('./games/games.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(user);
+  app.configure(games);
 }
