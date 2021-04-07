e0afeeaf7de9d5d6201b1be2e7afa3812b700409
Popa Mihai <popamihai199@yahoo.com>
2021-04-07 20:32:13 +0300

    update the games table

--- src/models/games.model.js
+++ src/models/games.model.js
@@ -7,7 +7,8 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const schema = new Schema({
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    link: { type: String, required: true }
   }, {
     timestamps: true
   });
